c1788f0f72a091ebb661d4145831044ea113f715
gonzalocasado-davinci <gonzalo.casado@davinci.edu.ar>
2025-11-29 17:57:30 -0300

    feat: creacion carpeta frontend e inicio conexcion con backend

---
+++ frontend/src/api.js
@@ -0,0 +1,15 @@
+const API_URL = "http://localhost:3000/producots";
+
+export async function getProductos(){
+    const res = await fetch(API_URL);
+    return res.json();
+}
+
+export async function addProducto(producto) {
+    const res = await fetch(API_URL, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(producto)
+    });
+    return res.json();
+}
